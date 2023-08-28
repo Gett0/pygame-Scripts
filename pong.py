7aafd6ac4046f068c7ec86e8cb1f3244947317dc
Getto <gettopython@gmail.com>
2023-08-29 01:32:29 +0200

    Falta cambiar la velocidad de los jugadores

--- pong.py
+++ pong.py
@@ -10,11 +10,25 @@
 def marcador(marca_izq, marca_der):
 	# Marcador
 	font = pygame.font.SysFont("serif", 30) # Fuente
-	text = font.render(f"{marca_izq}    |    {marca_der}", True, white) # Texto
+	text = font.render(f"{marca_izq}         {marca_der}", True, white) # Texto
 	center_x = (screen_size[0] // 2) - (text.get_width() // 2) # posicion text
 	center_y = 20
 	return screen.blit(text, [center_x, center_y]) # ponerlo en pantalla
 
+# Para dibujar una línea discontinua
+def dib_lin_dis(surface, color, start_pos, end_pos, dash_length=10, gap_length=5):
+    x1, y1 = start_pos
+    x2, y2 = end_pos
+    dx = x2 - x1
+    dy = y2 - y1
+    distance = max(abs(dx), abs(dy))
+    dx /= distance
+    dy /= distance
+    x, y = x1, y1
+    for _ in range(int(distance / (dash_length + gap_length))):
+        pygame.draw.line(surface, color, (int(x), int(y)), (int(x + dx * dash_length), int(y + dy * dash_length)))
+        x += dx * (dash_length + gap_length)
+        y += dy * (dash_length + gap_length)
 
 
 
@@ -39,11 +53,15 @@ player1_x_coor = 50
 player1_y_coor = 300 - 45
 player1_y_speed = 0
 p1_mov_spe = 3
+p1_mov_spe_x = 3
+player1_x_speed = 3
 #Coordenadas y velocidad del jugador 2
 player2_x_coor = 750 - player_width
 player2_y_coor = 300 - 45
 player2_y_speed = 0
 p2_mov_spe = 3
+p2_mov_spe_x = 3
+player2_x_speed = 3
 
 # Coordenadas de la pelota
 pelota_x = 400
@@ -64,11 +82,19 @@ while not game_over:
 				player1_y_speed = -p1_mov_spe
 			if event.key == pygame.K_s:
 				player1_y_speed = p1_mov_spe
+			if event.key == pygame.K_a:
+				player1_x_speed = -p1_mov_spe_x
+			if event.key == pygame.K_d:
+				player1_x_speed = p1_mov_spe_x
 			# Jugador 2
 			if event.key == pygame.K_UP:
 				player2_y_speed = -p2_mov_spe
 			if event.key == pygame.K_DOWN:
 				player2_y_speed = p2_mov_spe
+			if event.key == pygame.K_LEFT:
+				player2_x_speed = -p2_mov_spe_x
+			if event.key == pygame.K_RIGHT:
+				player2_x_speed = p2_mov_spe_x
 
 		if event.type == pygame.KEYUP:
 			# Jugador 1
@@ -76,27 +102,48 @@ while not game_over:
 				player1_y_speed = 0
 			if event.key == pygame.K_s:
 				player1_y_speed = 0
+			if event.key == pygame.K_a:
+				player1_x_speed = 0
+			if event.key == pygame.K_d:
+				player1_x_speed = 0
 			# Jugador 2
 			if event.key == pygame.K_UP:
 				player2_y_speed = 0
 			if event.key == pygame.K_DOWN:
 				player2_y_speed = 0
+			if event.key == pygame.K_LEFT:
+				player2_x_speed = 0
+			if event.key == pygame.K_RIGHT:
+				player2_x_speed = 0
 
 	if pelota_y > 590 or pelota_y < 10:
 		pelota_speed_y *= -1
 
 	# Control de jugadores que no se salgan de los límites de la pantalla
+	if player1_x_coor < 5:
+		player1_x_coor = 5
+
+	if player1_x_coor > screen_size[0] // 2 - 20:
+		player1_x_coor = screen_size[0] // 2 - 20
+
 	if player1_y_coor > 595:
 		player1_y_coor = 595
 	
 	if player1_y_coor < -85:
 		player1_y_coor = -85
 	
+	if player2_x_coor < screen_size[0] // 2 + 5:
+		player2_x_coor = screen_size[0] // 2 + 5
+
+	if player2_x_coor > screen_size[0] - 20:
+		player2_x_coor = screen_size[0] - 20
+	
 	if player2_y_coor > 595:
 		player2_y_coor = 595
 	
 	if player2_y_coor < -85:
 		player2_y_coor = -85
+
 
 	# Revisa si la pelota sale del lado derecho
 	if pelota_x > 800:
@@ -105,6 +152,8 @@ while not game_over:
 		marca_der += 1
 		pelota_speed_x = 3
 		pelota_speed_y = 3
+		player1_y_speed = 3
+		player2_y_speed = 3
 		
 		# Si sale de la pantalla, invierte direccion
 		pelota_speed_x *= -1
@@ -117,6 +166,8 @@ while not game_over:
 		marca_izq += 1
 		pelota_speed_x = 3
 		pelota_speed_y = 3
+		player1_y_speed = 3
+		player2_y_speed = 3
 		
 		# Si sale de la pantalla, invierte direccion
 		pelota_speed_x *= -1
@@ -126,6 +177,9 @@ while not game_over:
 	# Modifica las coordenadas para dar mov. a los jugadores/ pelota
 	player1_y_coor += player1_y_speed
 	player2_y_coor += player2_y_speed
+	player1_x_coor += player1_x_speed
+	player2_x_coor += player2_x_speed
+
 	# Movimiento pelota
 	pelota_x += pelota_speed_x
 	pelota_y += pelota_speed_y
@@ -135,6 +189,7 @@ while not game_over:
 	jugador1 = pygame.draw.rect(screen, white, (player1_x_coor, player1_y_coor, player_width, player_height))
 	jugador2 = pygame.draw.rect(screen, white, (player2_x_coor, player2_y_coor, player_width, player_height))
 	marcador(marca_der, marca_izq)
+	dib_lin_dis(screen, white, (screen_size[0] // 2, 0), (screen_size[0] // 2, screen_size[1]))
 	pelota = pygame.draw.circle(screen, white, (pelota_x, pelota_y), 10)
 
 
